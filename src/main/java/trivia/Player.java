242b221c2cee3ed35638a9a977ef4f1e74e18d53
horatiu.dan <horatiu.dan@tangoe.com>
2024-05-27 09:27:20 +0300

    Re-formats code to be mode readable.

--- src/main/java/trivia/Player.java
+++ src/main/java/trivia/Player.java
@@ -1,44 +1,44 @@
 package trivia;
 
 public class Player {
-  private final String name;
-  private int position;
-  private int coins;
-  private boolean isInPenaltyBox;
 
-  public Player(String name) {
-    this.name = name;
-  }
+    private final String name;
+    private int position;
+    private int coins;
+    private boolean isInPenaltyBox;
 
-  public void rollPosition(int roll, int totalNumberOfPositions) {
-    this.position += roll;
-    if (this.position > totalNumberOfPositions - 1) {
-      this.position -= totalNumberOfPositions;
+    public Player(String name) {
+        this.name = name;
     }
-  }
 
+    public void rollPosition(int roll, int totalNumberOfPositions) {
+        this.position += roll;
+        if (this.position > totalNumberOfPositions - 1) {
+            this.position -= totalNumberOfPositions;
+        }
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public int getPosition() {
-    return position;
-  }
+    public int getPosition() {
+        return position;
+    }
 
-  public int getCoins() {
-    return coins;
-  }
+    public int getCoins() {
+        return coins;
+    }
 
-  public boolean isInPenaltyBox() {
-    return isInPenaltyBox;
-  }
+    public boolean isInPenaltyBox() {
+        return isInPenaltyBox;
+    }
 
-  public void setInPenaltyBox(boolean inPenaltyBox) {
-    isInPenaltyBox = inPenaltyBox;
-  }
+    public void setInPenaltyBox(boolean inPenaltyBox) {
+        isInPenaltyBox = inPenaltyBox;
+    }
 
-  void addCoin() {
-      coins++;
-  }
+    void addCoin() {
+        coins++;
+    }
 }

6acc7e61b8cfecd7109294322ec9a9c530e72a59
horatiu.dan <horatiu.dan@tangoe.com>
2024-05-27 09:49:08 +0300

    Improves Questions.

--- src/main/java/trivia/Questions.java
+++ src/main/java/trivia/Questions.java
@@ -1,35 +1,27 @@
 package trivia;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 public class Questions {
 
-    public static final int NUMBER_OF_CATEGORIES = 4;
+    private final Map<Category, LinkedList<String>> questionsMap = new EnumMap<>(Category.class);
 
-    private final Map<Category, LinkedList<String>> questionsMap =
-            new HashMap<>(NUMBER_OF_CATEGORIES);
-
-
-
-
-    public Questions(){
+    public Questions() {
         for (Category category : Category.values()) {
             questionsMap.put(category, new LinkedList<>());
             for (int i = 0; i < 50; i++) {
                 questionsMap.get(category).add(createQuestion(i, category.name()));
             }
         }
-
     }
 
-    public String getQuestion(Category category){
+    public String getQuestion(Category category) {
         return questionsMap.get(category).removeFirst();
     }
 
-    public String createQuestion(int index, String category){
+    public String createQuestion(int index, String category) {
         return category + " Question " + index;
     }
-
 }
